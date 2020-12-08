2d4fda30358aedda64ebb0b8068e864a6ab945d8
Christian Canham <44207713+CRCanham@users.noreply.github.com>
2020-12-07 20:46:00 -0800

    Add Embossed Text
    Added blender automation to add latitude and longitude embossed text on the bottom of each puzzle piece

--- Blender_Automate.py
+++ Blender_Automate.py
@@ -7,9 +7,9 @@ lng = 0
 @persistent
 def load_handler(dummy):
 	bpy.ops.wm.save_as_mainfile(filepath="C:\\Users\\chris\\OneDrive\\Documents\\Blender\\Python_test4.blend")
-    bpy.ops.import_mesh.stl(filepath="C:\\Users\\chris\\OneDrive\\Documents\\Terrain\\stls\\N" + str(lat) + "_W" + str(lng) + ".stl")
-    bpy.context.object.dimensions = [50, 65, 1]
-    bpy.context.object.scale[2] = 0.14
+    	bpy.ops.import_mesh.stl(filepath="C:\\Users\\chris\\OneDrive\\Documents\\Terrain\\stls\\N" + str(lat) + "_W" + str(lng) + ".stl")
+    	bpy.context.object.dimensions = [50, 65, 1]
+    	bpy.context.object.scale[2] = 0.18
 	
 	# Join the two objects
 	#---------------------------------------------------
@@ -27,7 +27,39 @@ def load_handler(dummy):
 	bpy.ops.object.join(ctx)
 	#---------------------------------------------------
 	
-	bpy.ops.export_mesh.stl(filepath="C:\\Users\\chris\\OneDrive\\Documents\\Terrain\\Complete\\" + ob.name + ".stl")
+	# Add Embossed Text
+	#---------------------------------------------------
+	bpy.ops.object.text_add(enter_editmode=False, location=(25, -8, -5))
+	bpy.context.object.scale = 6, 6, 6
+	bpy.context.object.rotation_euler[1] = math.pi
+
+
+	bpy.ops.object.editmode_toggle()
+	bpy.ops.font.delete(type='PREVIOUS_OR_SELECTION')
+	bpy.ops.font.delete(type='PREVIOUS_OR_SELECTION')
+	bpy.ops.font.delete(type='PREVIOUS_OR_SELECTION')
+	bpy.ops.font.delete(type='PREVIOUS_OR_SELECTION')
+	bpy.ops.font.text_insert(text="N"+str(lat)+"\nW"+str(lng))
+	bpy.ops.object.editmode_toggle()
+
+	bpy.context.object.data.extrude = 0.4
+	bpy.ops.object.convert(target='MESH')
+
+	scene = bpy.context.scene
+	obs = []
+	for ob in scene.objects:
+		if ob.type == 'MESH':
+			obs.append(ob)
+
+	bpy.context.view_layer.objects.active = obs[0]
+	bpy.ops.object.modifier_add(type='BOOLEAN')
+	bpy.context.object.modifiers["Boolean"].object = bpy.data.objects["Text"]
+	bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Boolean")
+	bpy.context.view_layer.objects.active = obs[1]
+	bpy.ops.object.delete(use_global=False, confirm=False)
+	#---------------------------------------------------
+	
+	bpy.ops.export_mesh.stl(filepath="C:\\Users\\chris\\OneDrive\\Documents\\Terrain\\Colorado\\" +"N"+str(lat)+"_W"+str(lng) + ".stl")
 	
 		
     
